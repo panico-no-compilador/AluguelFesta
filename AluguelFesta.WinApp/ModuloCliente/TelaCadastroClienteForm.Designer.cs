dd413211fc63c2457933b50fc54e16ab42ca44d1
Fabian <Fabian@DESKTOP-H1BVHNB>
2023-06-12 16:42:27 -0300

    implementacao da tela cadastro cliente

---
+++ AluguelFesta.WinApp/ModuloCliente/TelaCadastroClienteForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace AluguelFesta.WinApp.ModuloCliente
+{
+    partial class TelaCadastroClienteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            lblNomeCliente = new Label();
+            btnCancelar = new Button();
+            btnGravar = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(79, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // lblNomeCliente
+            // 
+            lblNomeCliente.AutoSize = true;
+            lblNomeCliente.Location = new Point(30, 35);
+            lblNomeCliente.Name = "lblNomeCliente";
+            lblNomeCliente.Size = new Size(43, 15);
+            lblNomeCliente.TabIndex = 1;
+            lblNomeCliente.Text = "Nome:";
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnCancelar.Location = new Point(397, 114);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 40);
+            btnCancelar.TabIndex = 2;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            btnGravar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnGravar.Location = new Point(316, 114);
+            btnGravar.Name = "btnGravar";
+            btnGravar.Size = new Size(75, 40);
+            btnGravar.TabIndex = 3;
+            btnGravar.Text = "Gravar";
+            btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // TelaCadastroClienteForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 166);
+            Controls.Add(btnGravar);
+            Controls.Add(btnCancelar);
+            Controls.Add(lblNomeCliente);
+            Controls.Add(textBox1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaCadastroClienteForm";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cadastro Cliente";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label lblNomeCliente;
+        private Button btnCancelar;
+        private Button btnGravar;
+    }
+}
