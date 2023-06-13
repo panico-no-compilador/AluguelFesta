eb365ff96ce4f453934b94fcdcdcc1195bf10bc9
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 14:10:52 -0300

    adiciona class base para os controladores

---
+++ AluguelFesta.WinApp/Compartilhado/ControladorBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelFesta.WinApp.Compartilhado
+{
+    internal abstract class ControladorBase
+    {
+        public abstract string ToolTipInserir { get; }
+        public abstract string ToolTipEditar { get; }
+        public abstract string ToolTipExcluir { get; }
+
+
+        public virtual void Filtrar() { }
+        public virtual void Adicionar() { }
+        public abstract void Inserir();
+        public abstract void Editar();
+        public abstract void Excluir();
+        public abstract UserControl ObterListagem();
+        public abstract string ObterTipoCadastro();
+    }
+}
