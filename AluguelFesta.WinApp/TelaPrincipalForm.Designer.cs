50722cea0b8191274311fcf3ffea89f8f8b34102
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 14:15:30 -0300

    modifica nome dos ToolStripButton; modifica Data Grid View altera para Panel; modifica ToolStripMenuItem deleta cadastro de itens;

--- AluguelFesta.WinApp/TelaPrincipalForm.Designer.cs
+++ AluguelFesta.WinApp/TelaPrincipalForm.Designer.cs
@@ -32,20 +32,19 @@
             menuStrip1 = new MenuStrip();
             toolStripMenuItem1 = new ToolStripMenuItem();
             clienteMenuItem = new ToolStripMenuItem();
-            toolStrip1 = new ToolStrip();
-            toolStripInserir = new ToolStripButton();
-            toolStripEditar = new ToolStripButton();
-            toolStripDeletar = new ToolStripButton();
-            toolStripSeparator1 = new ToolStripSeparator();
-            toolStripInserirItens = new ToolStripButton();
-            toolStripTipoCadastro = new ToolStripLabel();
-            statusStrip1 = new StatusStrip();
-            dataGridView1 = new DataGridView();
             cadatroDeTemaToolStripMenuItem = new ToolStripMenuItem();
             cadastroDeAlugueisToolStripMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            btnInserir = new ToolStripButton();
+            btnEditar = new ToolStripButton();
+            btnExcluir = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            btnInserirItens = new ToolStripButton();
+            toolStripTipoCadastro = new ToolStripLabel();
+            statusStrip1 = new StatusStrip();
+            painelRegistros = new Panel();
             menuStrip1.SuspendLayout();
             toolStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
@@ -71,59 +70,76 @@
             clienteMenuItem.Text = "Cadastro de Cliente";
             clienteMenuItem.Click += clienteMenuItem_Click;
             // 
+            // cadatroDeTemaToolStripMenuItem
+            // 
+            cadatroDeTemaToolStripMenuItem.Name = "cadatroDeTemaToolStripMenuItem";
+            cadatroDeTemaToolStripMenuItem.Size = new Size(186, 22);
+            cadatroDeTemaToolStripMenuItem.Text = "Cadatro de Tema";
+            // 
+            // cadastroDeAlugueisToolStripMenuItem
+            // 
+            cadastroDeAlugueisToolStripMenuItem.Name = "cadastroDeAlugueisToolStripMenuItem";
+            cadastroDeAlugueisToolStripMenuItem.Size = new Size(186, 22);
+            cadastroDeAlugueisToolStripMenuItem.Text = "Cadastro de Alugueis";
+            // 
             // toolStrip1
             // 
-            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripInserir, toolStripEditar, toolStripDeletar, toolStripSeparator1, toolStripInserirItens, toolStripTipoCadastro });
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnInserir, btnEditar, btnExcluir, toolStripSeparator1, btnInserirItens, toolStripTipoCadastro });
             toolStrip1.Location = new Point(0, 24);
             toolStrip1.Name = "toolStrip1";
             toolStrip1.Size = new Size(450, 25);
             toolStrip1.TabIndex = 1;
             toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripInserir
+            // btnInserir
             // 
-            toolStripInserir.DisplayStyle = ToolStripItemDisplayStyle.Image;
-            toolStripInserir.Image = (Image)resources.GetObject("toolStripInserir.Image");
-            toolStripInserir.ImageTransparentColor = Color.Magenta;
-            toolStripInserir.Name = "toolStripInserir";
-            toolStripInserir.Size = new Size(23, 22);
-            toolStripInserir.Text = "btnInserir";
-            toolStripInserir.ToolTipText = "toolStripInserir";
+            btnInserir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnInserir.Enabled = false;
+            btnInserir.Image = (Image)resources.GetObject("btnInserir.Image");
+            btnInserir.ImageTransparentColor = Color.Magenta;
+            btnInserir.Name = "btnInserir";
+            btnInserir.Size = new Size(23, 22);
+            btnInserir.Text = "Inserir";
+            btnInserir.ToolTipText = "toolStripInserir";
+            btnInserir.Click += btnInserir_Click;
             // 
-            // toolStripEditar
+            // btnEditar
             // 
-            toolStripEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
-            toolStripEditar.Image = (Image)resources.GetObject("toolStripEditar.Image");
-            toolStripEditar.ImageTransparentColor = Color.Magenta;
-            toolStripEditar.Name = "toolStripEditar";
-            toolStripEditar.Size = new Size(23, 22);
-            toolStripEditar.Text = "btnEditar";
-            toolStripEditar.ToolTipText = "toolStripEditar";
+            btnEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnEditar.Enabled = false;
+            btnEditar.Image = (Image)resources.GetObject("btnEditar.Image");
+            btnEditar.ImageTransparentColor = Color.Magenta;
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(23, 22);
+            btnEditar.Text = "Editar";
+            btnEditar.ToolTipText = "toolStripEditar";
             // 
-            // toolStripDeletar
+            // btnExcluir
             // 
-            toolStripDeletar.DisplayStyle = ToolStripItemDisplayStyle.Image;
-            toolStripDeletar.Image = (Image)resources.GetObject("toolStripDeletar.Image");
-            toolStripDeletar.ImageTransparentColor = Color.Magenta;
-            toolStripDeletar.Name = "toolStripDeletar";
-            toolStripDeletar.Size = new Size(23, 22);
-            toolStripDeletar.Text = "btnDeletar";
-            toolStripDeletar.ToolTipText = "toolStripDeletar";
+            btnExcluir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnExcluir.Enabled = false;
+            btnExcluir.Image = (Image)resources.GetObject("btnExcluir.Image");
+            btnExcluir.ImageTransparentColor = Color.Magenta;
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(23, 22);
+            btnExcluir.Text = "Excluir";
+            btnExcluir.ToolTipText = "toolStripExcluir";
             // 
             // toolStripSeparator1
             // 
             toolStripSeparator1.Name = "toolStripSeparator1";
             toolStripSeparator1.Size = new Size(6, 25);
             // 
-            // toolStripInserirItens
+            // btnInserirItens
             // 
-            toolStripInserirItens.DisplayStyle = ToolStripItemDisplayStyle.Image;
-            toolStripInserirItens.Image = (Image)resources.GetObject("toolStripInserirItens.Image");
-            toolStripInserirItens.ImageTransparentColor = Color.Magenta;
-            toolStripInserirItens.Name = "toolStripInserirItens";
-            toolStripInserirItens.Size = new Size(23, 22);
-            toolStripInserirItens.Text = "btnInserirItem";
-            toolStripInserirItens.ToolTipText = "toolStripInserirItens";
+            btnInserirItens.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnInserirItens.Enabled = false;
+            btnInserirItens.Image = (Image)resources.GetObject("btnInserirItens.Image");
+            btnInserirItens.ImageTransparentColor = Color.Magenta;
+            btnInserirItens.Name = "btnInserirItens";
+            btnInserirItens.Size = new Size(23, 22);
+            btnInserirItens.Text = "Inserir Item";
+            btnInserirItens.ToolTipText = "toolStripInserirItens";
             // 
             // toolStripTipoCadastro
             // 
@@ -139,34 +155,20 @@
             statusStrip1.TabIndex = 2;
             statusStrip1.Text = "statusStrip1";
             // 
-            // dataGridView1
+            // painelRegistros
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(0, 49);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(450, 429);
-            dataGridView1.TabIndex = 3;
-            // 
-            // cadatroDeTemaToolStripMenuItem
-            // 
-            cadatroDeTemaToolStripMenuItem.Name = "cadatroDeTemaToolStripMenuItem";
-            cadatroDeTemaToolStripMenuItem.Size = new Size(186, 22);
-            cadatroDeTemaToolStripMenuItem.Text = "Cadatro de Tema";
-            // 
-            // cadastroDeAlugueisToolStripMenuItem
-            // 
-            cadastroDeAlugueisToolStripMenuItem.Name = "cadastroDeAlugueisToolStripMenuItem";
-            cadastroDeAlugueisToolStripMenuItem.Size = new Size(186, 22);
-            cadastroDeAlugueisToolStripMenuItem.Text = "Cadastro de Alugueis";
+            painelRegistros.Dock = DockStyle.Fill;
+            painelRegistros.Location = new Point(0, 49);
+            painelRegistros.Name = "painelRegistros";
+            painelRegistros.Size = new Size(450, 429);
+            painelRegistros.TabIndex = 3;
             // 
             // TelaPrincipalForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(450, 500);
-            Controls.Add(dataGridView1);
+            Controls.Add(painelRegistros);
             Controls.Add(statusStrip1);
             Controls.Add(toolStrip1);
             Controls.Add(menuStrip1);
@@ -178,7 +180,6 @@
             menuStrip1.PerformLayout();
             toolStrip1.ResumeLayout(false);
             toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -189,15 +190,15 @@
         private ToolStripMenuItem toolStripMenuItem1;
         private ToolStripMenuItem clienteMenuItem;
         private ToolStrip toolStrip1;
-        private ToolStripButton toolStripInserir;
-        private ToolStripButton toolStripEditar;
-        private ToolStripButton toolStripDeletar;
+        private ToolStripButton btnInserir;
+        private ToolStripButton btnEditar;
+        private ToolStripButton btnExcluir;
         private ToolStripSeparator toolStripSeparator1;
-        private ToolStripButton toolStripInserirItens;
+        private ToolStripButton btnInserirItens;
         private ToolStripLabel toolStripTipoCadastro;
         private StatusStrip statusStrip1;
-        private DataGridView dataGridView1;
         private ToolStripMenuItem cadatroDeTemaToolStripMenuItem;
         private ToolStripMenuItem cadastroDeAlugueisToolStripMenuItem;
+        private Panel painelRegistros;
     }
 }
