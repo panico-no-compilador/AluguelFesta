f54912061f5b781481a23c901b66ddae593ff605
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 15:47:44 -0300

    adciona ctor e implementa metodos atualizar informacoes e validar

--- AluguelFesta.Dominio/ModuloAluguel/Aluguel.cs
+++ AluguelFesta.Dominio/ModuloAluguel/Aluguel.cs
@@ -11,15 +11,39 @@ namespace AluguelFesta.Dominio.ModuloAluguel
         public decimal ValorTotal { get; set; }
         public Cliente Cliente { get; set; }
         public Tema Tema { get; set; }
+        public Aluguel()
+        {
+            
+        }
+
+        public Aluguel(int desconto, decimal valorEntrada, decimal valorTotal, Cliente cliente, Tema tema)
+        {
+            this.Desconto = desconto;
+            this.ValorEntrada = valorEntrada;
+            this.ValorTotal = valorTotal;
+            this.Cliente = cliente;
+            this.Tema = tema;
+        }
 
         public override void AtualizarInformacoes(Aluguel registroAtualizado)
         {
-            throw new NotImplementedException();
+            this.Desconto = registroAtualizado.Desconto;
+            this.ValorEntrada = registroAtualizado.ValorEntrada;
+            this.ValorTotal = registroAtualizado.ValorTotal;
+            this.Cliente = registroAtualizado.Cliente;
+            this.Tema = registroAtualizado.Tema;
         }
 
         public override string[] Validar()
         {
-            throw new NotImplementedException();
+            List<string> erros = new List<string>();
+            if (ValorEntrada == null)
+                erros.Add("O campo 'ValorEntrada' é obrigatório");
+            if (Cliente == null)
+                erros.Add("O campo 'Cliente' é obrigatório");
+            if (Tema == null)
+                erros.Add("O campo 'Tema' é obrigatório");
+            return erros.ToArray();
         }
     }
 }
