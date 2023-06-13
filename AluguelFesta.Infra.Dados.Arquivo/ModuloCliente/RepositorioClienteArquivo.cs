54a6a1c7a6e2e97028ab061f1d33cfb60db997a2
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 14:09:21 -0300

    cria repositorio para salvar em arquivos;  adiciona heranca com repositorio arquivo base;  adiciona interface repositorio base;  adicionar ctor;  adiciona heranca para o ctor que vem do repositorio base;

---
+++ AluguelFesta.Infra.Dados.Arquivo/ModuloCliente/RepositorioClienteArquivo.cs
@@ -0,0 +1,24 @@
+﻿using AluguelFesta.Dominio.ModuloCliente;
+using AluguelFesta.Infra.Dados.Arquivo.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelFesta.Infra.Dados.Arquivo.ModuloCliente
+{
+    public class RepositorioClienteArquivo : RepositorioArquivoBase<Cliente>, IRepositorioCliente
+    {
+        public RepositorioClienteArquivo(ContextoDados contexto) : base(contexto)
+        {
+
+        }
+
+
+        protected override List<Cliente> ObterRegistros()
+        {
+            return contextoDados.clientes;
+        }
+    }
+}
