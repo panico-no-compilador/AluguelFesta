15ea69de905fa346dc817d3f218f390305c2b7f7
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 14:14:45 -0300

    adiciona instancias do contexto de dados e repositorio do cliente em arquivo; adiciona metodo de click no menu item para navegacao para abrir cadastro cliente; adiciona metodo para configurar os toolstriptext dos btn;  adiciona metodo para obter listagem do controlador e adicionar no painel de registros; adiciona metodo para habilitar o click nos btn do crud na tela principal; adiciona metodo para habilitar o click nos btn de adicionar item na tela principal; modifica remove metodo ConfigurarDialog do ctor;  modifica remove metodo Form1_Load

--- AluguelFesta.WinApp/TelaPrincipalForm.cs
+++ AluguelFesta.WinApp/TelaPrincipalForm.cs
@@ -1,33 +1,76 @@
+using AluguelFesta.Dominio.ModuloCliente;
+using AluguelFesta.Infra.Dados.Arquivo.Compartilhado;
 using AluguelFesta.WinApp.Compartilhado;
+using AluguelFesta.WinApp.ModuloCliente;
+using AluguelFesta.Infra.Dados.Arquivo.ModuloCliente;
 
 namespace AluguelFesta.WinApp
 {
     public partial class TelaPrincipalForm : Form
     {
+        private static TelaPrincipalForm telaPrincipal;
+        private ControladorBase controlador;
+        private static ContextoDados contextoDados = new ContextoDados(carregarDados: true);
+        private IRepositorioCliente repositorioCliente = new RepositorioClienteArquivo(contextoDados);
         public TelaPrincipalForm()
         {
             InitializeComponent();
-            this.ConfigurarDialog();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        public static TelaPrincipalForm Instancia
         {
-
+            get
+            {
+                if (telaPrincipal == null)
+                    telaPrincipal = new TelaPrincipalForm();
+                return telaPrincipal;
+            }
         }
-
         private void clienteMenuItem_Click(object sender, EventArgs e)
         {
-
+            controlador = new ControladorCliente(repositorioCliente);
+            ConfigurarTelaPrincipal(controlador);
+            HabilitarBotoesCrud(false);
         }
 
-        private void alugarMenuItem_Click(object sender, EventArgs e)
-        {
 
+        private void ConfigurarTelaPrincipal(ControladorBase controladorBase)
+        {
+            toolStripTipoCadastro.Text = controladorBase.ObterTipoCadastro();
+            ConfigurarToolTips(controlador);
+            ConfigurarListagem(controlador);
+        }
+        private void ConfigurarToolTips(ControladorBase controlador)
+        {
+            btnInserir.ToolTipText = controlador.ToolTipInserir;
+            btnEditar.ToolTipText = controlador.ToolTipEditar;
+            btnExcluir.ToolTipText = controlador.ToolTipExcluir;
+        }
+        private void ConfigurarListagem(ControladorBase controladorBase)
+        {
+            UserControl listagem = controladorBase.ObterListagem();
+            listagem.Dock = DockStyle.Fill;
+            painelRegistros.Controls.Clear();
+            painelRegistros.Controls.Add(listagem);
+        }
+        private void HabilitarBotoesCrud(bool temItem)
+        {
+            btnEditar.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnInserir.Enabled = true;
+
+            if (temItem)
+            {
+                btnInserirItens.Enabled = true;
+            }
+            else
+            {
+                btnInserirItens.Enabled = false;
+            }
         }
 
-        private void cadastrarItensMenuItem_Click(object sender, EventArgs e)
+        private void btnInserir_Click(object sender, EventArgs e)
         {
-
+            controlador.Inserir();
         }
     }
 }
