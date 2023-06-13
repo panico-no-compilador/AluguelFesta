fd17c0460386e77533395f307976bba8a78e258f
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 14:04:29 -0300

    adiciona interface do repositorio do cliente;

---
+++ AluguelFesta.Dominio/ModuloCliente/IRepositorioCliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelFesta.Dominio.ModuloCliente
+{
+    public interface IRepositorioCliente
+    {
+        void Inserir(Cliente novoCliente);
+        void Editar(int id, Cliente cliente);
+        void Excluir(Cliente clienteSelecionado);
+        List<Cliente> SelecionarTodos();
+        Cliente SelecionarPorId(int id);
+    }
+}
