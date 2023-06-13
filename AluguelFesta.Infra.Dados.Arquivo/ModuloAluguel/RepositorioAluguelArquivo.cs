5e1a6acb7bd44edacad9e95c5ec35ed344f3b741
Davi Borges <davifelipe_300@hotmail.com>
2023-06-13 15:45:47 -0300

    adiciona repositorio aluguel em arquivo para trabalhar com json

---
+++ AluguelFesta.Infra.Dados.Arquivo/ModuloAluguel/RepositorioAluguelArquivo.cs
@@ -0,0 +1,16 @@
+﻿using AluguelFesta.Dominio.ModuloAluguel;
+
+namespace AluguelFesta.Infra.Dados.Arquivo.ModuloAluguel
+{
+    internal class RepositorioAluguelArquivo : RepositorioArquivoBase<Aluguel>
+    {
+        public RepositorioAluguelArquivo(ContextoDados contexto) : base(contexto)
+        {
+
+        }
+        protected override List<Aluguel> ObterRegistros()
+        {
+            return contextoDados.alugueis;
+        }
+    }
+}
