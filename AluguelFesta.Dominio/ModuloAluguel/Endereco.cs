67d36a4810dae05393998675cc52b654a101a843
Davi Borges <davifelipe_300@hotmail.com>
2023-06-14 17:50:09 -0300

    adiciona as class endereco e festa na pasta do modulo aluguel

---
+++ AluguelFesta.Dominio/ModuloAluguel/Endereco.cs
@@ -0,0 +1,10 @@
+﻿namespace AluguelFesta.Dominio.ModuloAluguel
+{
+    [Serializable]
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Cidade { get; set; }
+    }
+}
---
+++ AluguelFesta.Dominio/ModuloAluguel/Festa.cs
@@ -0,0 +1,20 @@
+﻿namespace AluguelFesta.Dominio.ModuloAluguel
+{
+    [Serializable]
+    public class Festa : EntidadeBase<Festa>
+    {
+        TimeSpan InicioFesta { get; set; }
+        TimeSpan TerminoFesta { get; set; }
+        public Endereco Endereco { get; set; }
+
+        public override void AtualizarInformacoes(Festa registroAtualizado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
