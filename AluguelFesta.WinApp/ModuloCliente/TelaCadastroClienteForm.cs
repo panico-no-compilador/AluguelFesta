5ff34a789d860fddd33281e93e229b4123f5ce78
Fabian <Fabian@DESKTOP-H1BVHNB>
2023-06-14 17:46:32 -0300

    adiciona propriedades na tela do cliente

--- AluguelFesta.WinApp/ModuloCliente/TelaCadastroClienteForm.cs
+++ AluguelFesta.WinApp/ModuloCliente/TelaCadastroClienteForm.cs
@@ -7,16 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AluguelFesta.Dominio.ModuloCliente;
 using AluguelFesta.WinApp.Compartilhado;
 
 namespace AluguelFesta.WinApp.ModuloCliente
 {
     public partial class TelaCadastroClienteForm : Form
     {
+        Cliente cliente;
         public TelaCadastroClienteForm()
         {
             InitializeComponent();
             this.ConfigurarDialog();
+        }
+        public Cliente Cliente
+        {
+            get
+            {
+                return this.cliente;
+            }
+            set
+            {
+                tboxNome.Text = value.Nome;
+                tboxTelefone.Text = value.Telefone.ToString();
+                tboxEmail.Text = value.Email;
+            }
+
+        }
+        public Cliente ObterCliente()
+        {
+
+            string nome = tboxNome.Text;
+            int telefone = (int)Convert.ToInt32(tboxTelefone.Text);
+            string email = tboxEmail.Text;
+            Cliente cliente = new Cliente(nome, telefone, email);
+            return cliente;
+        }
+
+        public void ConfigurarTela(Cliente cliente)
+        {
+            tboxNome.Text = cliente.Nome;
+            tboxTelefone.Text = cliente.Telefone.ToString();
+            tboxEmail.Text = cliente.Email;
+        }
+        private void btnGravar_Click_1(object sender, EventArgs e)
+        {
+            string nome = tboxNome.Text;
+            int telefone = Convert.ToInt32(tboxTelefone.Text);
+            string email = tboxEmail.Text;
+            cliente = new Cliente(nome, telefone, email);
         }
     }
 }
