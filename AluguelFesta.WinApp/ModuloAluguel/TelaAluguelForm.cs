a7e4a75fea39c5a84a0d5e71e6d4a7d71f6bd9b9
Davi Borges <davifelipe_300@hotmail.com>
2023-06-15 11:31:03 -0300

    modifica tela aluguel

--- AluguelFesta.WinApp/ModuloAluguel/TelaAluguelForm.cs
+++ AluguelFesta.WinApp/ModuloAluguel/TelaAluguelForm.cs
@@ -2,16 +2,32 @@
 using AluguelFesta.Dominio.ModuloCliente;
 using AluguelFesta.Dominio.ModuloTema;
 using AluguelFesta.WinApp.Compartilhado;
+using System.Collections;
 
 namespace AluguelFesta.WinApp.ModuloAluguel
 {
     public partial class TelaAluguelForm : Form
     {
         Aluguel aluguel;
-        public TelaAluguelForm()
+        IRepositorioCliente repositorioCliente;
+        public TelaAluguelForm(List<Cliente> clientes)
         {
+            //this.repositorioCliente = _repositorioCliente;
             InitializeComponent();
             this.ConfigurarDialog();
+            PopularComboBox(clientes);
+        }
+        private void PopularComboBox(List<Cliente> clientes)
+        {
+            cboxClientes.Items.Clear();
+            foreach (Cliente cliente in clientes)
+            {
+                cboxClientes.Items.Add(cliente);
+            }
+            //cboxClientes.DataSource = repositorioCliente.SelecionarTodos();
+            //cboxTema.DataSource = repositorioTema.SelecionarTodos();
+            cboxClientes.DisplayMember = "Nome";
+            cboxTema.DisplayMember = "Nome";
         }
         public Aluguel Aluguel
         {
@@ -21,38 +37,56 @@ namespace AluguelFesta.WinApp.ModuloAluguel
             }
             set
             {
-                //cboxTema.Text = value.Tema.ToString();
-                //txtTelefone.Text = value.Telefone.ToString();
-                //txtEmail.Text = value.Email;
+                cboxClientes.DataSource = repositorioCliente.SelecionarTodos();
+                //TO-DO
             }
 
         }
         public Aluguel ObterAluguel()
         {
-            Tema tema = null; //funcao para obter lista de tema
-            Cliente cliente = null; //funcao para obter lista de tema
             decimal entrada = Convert.ToDecimal(tbValorEntrada.Text);
             decimal total = Convert.ToDecimal(tbValorTotal.Text);
             decimal desconto = Convert.ToInt32(tbValorTotal.Text);
-            aluguel = new Aluguel(desconto, entrada, total, cliente, tema);
+            Cliente cliente = (Cliente)cboxClientes.SelectedItem;
+            Tema tema = (Tema)cboxTema.SelectedItem;
+            Festa festa = new Festa(
+                Convert.ToDateTime(tbHorarioInicio.Text),
+                Convert.ToDateTime(tbHorarioTermino.Text),
+                new Endereco(
+                    tbRua.Text,
+                    Convert.ToInt32(tbNumeroCasaApto.Text),
+                    tbCidade.Text,
+                    tbEstado.Text
+                    )
+                );
+            aluguel = new Aluguel(desconto, entrada, total, cliente, festa, tema);
             return aluguel;
         }
 
-        public void ConfigurarTela(Aluguel cliente)
+        public void ConfigurarTela(Aluguel aluguelSelecionado)
         {
-            txtNome.Text = cliente.Nome;
-            txtTelefone.Text = cliente.Telefone.ToString();
-            txtEmail.Text = cliente.Email;
+            //txtNome.Text = cliente.Nome;
+            //txtTelefone.Text = cliente.Telefone.ToString();
+            //txtEmail.Text = cliente.Email;
         }
         private void btnGravar_Click(object sender, EventArgs e)
         {
-            Tema tema = cboxTema.SelectedItem as Tema;
-            Cliente cliente = cboxClientes.SelectedItem as Cliente;
-            decimal entrada = 500; //40 ou 50% do total
+            //Tema tema = (Tema)cboxTema.SelectedValue;
+            Cliente cliente = (Cliente)cboxClientes.SelectedValue;
+            decimal entrada = 500;
             decimal total = 1000;
+            Festa festa = new Festa(
+                Convert.ToDateTime(tbHorarioInicio.Text),
+                Convert.ToDateTime(tbHorarioTermino.Text),
+                new Endereco(
+                    tbRua.Text,
+                    Convert.ToInt32(tbNumeroCasaApto.Text),
+                    tbCidade.Text,
+                    tbEstado.Text)
+                );
 
             decimal desconto = (Convert.ToDecimal(cliente.HistoricoAlugueis.Count) / 100) * total;
-            aluguel = new Aluguel(desconto, entrada, total, cliente, tema);
+            aluguel = new Aluguel(desconto, entrada, total, cliente, festa);
         }
     }
 }
