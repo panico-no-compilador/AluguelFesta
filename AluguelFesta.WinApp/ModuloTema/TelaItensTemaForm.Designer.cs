1ab46198321b707f53935fb2971bf8c406711df7
Davi Borges <davifelipe_300@hotmail.com>
2023-06-16 16:36:55 -0300

    adiciona tela cadastro itens do tema

---
+++ AluguelFesta.WinApp/ModuloTema/TelaItensTemaForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace AluguelFesta.WinApp.ModuloTema
+{
+    partial class TelaItensTemaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            tbNomeTema = new TextBox();
+            label2 = new Label();
+            tbItem = new TextBox();
+            label3 = new Label();
+            tbValorItem = new TextBox();
+            btnAdicionarItem = new Button();
+            btnDeletarItem = new Button();
+            btnCancelar = new Button();
+            btnGravar = new Button();
+            gridItens = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)gridItens).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Tema:";
+            // 
+            // tbNomeTema
+            // 
+            tbNomeTema.Location = new Point(72, 39);
+            tbNomeTema.Name = "tbNomeTema";
+            tbNomeTema.ReadOnly = true;
+            tbNomeTema.Size = new Size(239, 23);
+            tbNomeTema.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(34, 15);
+            label2.TabIndex = 18;
+            label2.Text = "Item:";
+            // 
+            // tbItem
+            // 
+            tbItem.Location = new Point(72, 83);
+            tbItem.Name = "tbItem";
+            tbItem.Size = new Size(239, 23);
+            tbItem.TabIndex = 19;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(28, 129);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 20;
+            label3.Text = "Valor:";
+            // 
+            // tbValorItem
+            // 
+            tbValorItem.Location = new Point(72, 126);
+            tbValorItem.Name = "tbValorItem";
+            tbValorItem.Size = new Size(77, 23);
+            tbValorItem.TabIndex = 21;
+            // 
+            // btnAdicionarItem
+            // 
+            btnAdicionarItem.BackColor = Color.PaleGreen;
+            btnAdicionarItem.Location = new Point(155, 126);
+            btnAdicionarItem.Name = "btnAdicionarItem";
+            btnAdicionarItem.Size = new Size(75, 26);
+            btnAdicionarItem.TabIndex = 24;
+            btnAdicionarItem.Text = "Adicionar";
+            btnAdicionarItem.UseVisualStyleBackColor = false;
+            btnAdicionarItem.Click += btnAdicionarItem_Click;
+            // 
+            // btnDeletarItem
+            // 
+            btnDeletarItem.BackColor = Color.LightCoral;
+            btnDeletarItem.Location = new Point(236, 126);
+            btnDeletarItem.Name = "btnDeletarItem";
+            btnDeletarItem.Size = new Size(75, 26);
+            btnDeletarItem.TabIndex = 25;
+            btnDeletarItem.Text = "Deletar";
+            btnDeletarItem.UseVisualStyleBackColor = false;
+            btnDeletarItem.Click += btnDeletarItem_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancelar.DialogResult = DialogResult.Cancel;
+            btnCancelar.Location = new Point(236, 378);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 41);
+            btnCancelar.TabIndex = 28;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            btnGravar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGravar.DialogResult = DialogResult.OK;
+            btnGravar.Location = new Point(155, 378);
+            btnGravar.Name = "btnGravar";
+            btnGravar.Size = new Size(75, 41);
+            btnGravar.TabIndex = 27;
+            btnGravar.Text = "Gravar";
+            btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // gridItens
+            // 
+            gridItens.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gridItens.Location = new Point(28, 189);
+            gridItens.Name = "gridItens";
+            gridItens.RowTemplate.Height = 25;
+            gridItens.Size = new Size(283, 163);
+            gridItens.TabIndex = 29;
+            // 
+            // TelaItensTemaForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(339, 440);
+            Controls.Add(gridItens);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnGravar);
+            Controls.Add(btnDeletarItem);
+            Controls.Add(btnAdicionarItem);
+            Controls.Add(tbValorItem);
+            Controls.Add(label3);
+            Controls.Add(tbItem);
+            Controls.Add(label2);
+            Controls.Add(tbNomeTema);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaItensTemaForm";
+            ShowIcon = false;
+            Text = "Cadastro Itens do Tema";
+            ((System.ComponentModel.ISupportInitialize)gridItens).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox tbNomeTema;
+        private Label label2;
+        private TextBox tbItem;
+        private Label label3;
+        private TextBox tbValorItem;
+        private Button btnAdicionarItem;
+        private Button btnDeletarItem;
+        private Button btnCancelar;
+        private Button btnGravar;
+        private DataGridView gridItens;
+    }
+}
---
+++ AluguelFesta.WinApp/ModuloTema/TelaItensTemaForm.cs
@@ -0,0 +1,136 @@
+﻿using AluguelFesta.Dominio.ModuloTema;
+using AluguelFesta.WinApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AluguelFesta.WinApp.ModuloTema
+{
+    public partial class TelaItensTemaForm : Form
+    {
+        Item item;
+        Tema tema;
+        public TelaItensTemaForm(Tema tema)
+        {
+            this.tema = tema;
+            InitializeComponent();
+            ConfigurarTela(tema);
+            ConfigurarColunas();
+            gridItens.ConfigurarGridSomenteLeitura();
+            gridItens.ConfigurarGridZebrado();
+            CarregarItem();
+        }
+        private void ConfigurarColunas()
+        {
+            DataGridViewColumn[] colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn()
+                {
+                    Name = "Nome",
+                    HeaderText = "Nome"
+                },
+                new DataGridViewTextBoxColumn()
+                {
+                    Name = "Valor",
+                    HeaderText = "Valor"
+                }
+            };
+            gridItens.Columns.AddRange(colunas);
+        }
+        public Tema Tema
+        {
+            get
+            {
+                return this.tema;
+            }
+            set
+            {
+                tbNomeTema.Text = value.Nome;
+            }
+        }
+        public Item Item
+        {
+            get
+            {
+                return this.item;
+            }
+            set
+            {
+                tbItem.Text = value.Nome;
+                tbValorItem.Text = value.Valor.ToString();
+            }
+        }
+        public void AtualizarRegistros(List<Item> itens)
+        {
+            gridItens.Rows.Clear();
+
+            foreach (Item item in itens)
+            {
+                gridItens.Rows.Add(
+                    item.Id,
+                    item.Nome,
+                    item.Valor
+                    );
+            }
+        }
+        public int ObterIdSelecionado()
+        {
+            int id;
+            try
+            {
+                id = Convert.ToInt32(gridItens.SelectedRows[0].Cells["Id"].Value);
+            }
+            catch
+            {
+                id = -1;
+            }
+            return id;
+        }
+
+        private void ConfigurarTela(Tema tema)
+        {
+            tbNomeTema.Text = tema.Nome;
+        }
+        public Item ObterItem()
+        {
+            string item = tbItem.Text;
+            decimal valor = Convert.ToDecimal(tbValorItem.Text);
+            return new Item(item, valor);
+        }
+        private void btnAdicionarItem_Click(object sender, EventArgs e)
+        {
+            this.item = ObterItem();
+            string[] erros = item.Validar();
+            Tema.Itens.Add(this.item);
+            AtualizarRegistros(Tema.Itens);
+            CarregarItem();
+        }
+
+        private void CarregarItem()
+        {
+            gridItens.Rows.Clear();
+            foreach (Item item in Tema.Itens)
+            {
+                gridItens.Rows.Add(
+                    item.Nome,
+                    item.Valor
+                    );
+            }
+        }
+
+        private void btnDeletarItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //public List<Item> ObterItensCadastrados()
+        //{
+        //    return tabelaItens.Items.Cast<Item>().ToList();
+        //}
+    }
+}
