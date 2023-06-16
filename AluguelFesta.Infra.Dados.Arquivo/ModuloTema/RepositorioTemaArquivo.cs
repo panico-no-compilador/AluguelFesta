e99e096440865c8d2717c1086098880d23a49ea5
Davi Borges <davifelipe_300@hotmail.com>
2023-06-16 00:55:11 -0300

    adiciona o repositorio de tema

---
+++ AluguelFesta.Infra.Dados.Arquivo/ModuloTema/RepositorioTemaArquivo.cs
@@ -0,0 +1,15 @@
+﻿using AluguelFesta.Dominio.ModuloTema;
+
+namespace AluguelFesta.Infra.Dados.Arquivo.ModuloTema
+{
+    public class RepositorioTemaArquivo : RepositorioArquivoBase<Tema>, IRepositorioTema
+    {
+        public RepositorioTemaArquivo(ContextoDados contexto) : base(contexto)
+        {
+        }
+        protected override List<Tema> ObterRegistros()
+        {
+            return contextoDados.temas;
+        }
+    }
+}
